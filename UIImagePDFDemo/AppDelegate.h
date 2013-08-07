61c1c562cd39037bedc09bb0105672f8b54fc856
Kyle Lopez <kylealopez@gmail.com>
2013-08-07 11:21:09 -0700

    Initial Commit

---
+++ UIImagePDFDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  UIImagePDFDemo
+//
+//  Created by Kyle Lopez on 8/7/13.
+//  Copyright (c) 2013 Kyle Lopez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UIImagePDFDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  UIImagePDFDemo
+//
+//  Created by Kyle Lopez on 8/7/13.
+//  Copyright (c) 2013 Kyle Lopez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
